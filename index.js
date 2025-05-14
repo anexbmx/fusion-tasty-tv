27b6823b3453a3c89a812c8735cb05978968b6b5
anas boumediene <anexbm@gmail.com>
2025-05-14 17:13:08 +0200

    save

---
+++ index.js
@@ -0,0 +1,24 @@
+process.env.DISPLAY = ':0';
+
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+const imagePath = '/home/pi/fusion-tasty-tv/overload-fries.jpg';
+//const imagePath = '/home/pi/fusionTasty/overload-wraps.jpg';
+//const imagePath = '/home/pi/fusionTasty/overload-fries.jpg';
+
+// Check if the image file exists
+if (fs.existsSync(imagePath)) {
+  console.log('Image found, waiting 10 seconds to start...');
+
+  // Wait 10 seconds to make sure desktop is fully loaded
+  setTimeout(() => {
+    console.log('Displaying image now...');
+    spawn('feh', ['--fullscreen', '--hide-pointer', '--auto-zoom', imagePath], {
+      detached: true,
+      stdio: 'ignore'
+    }).unref();
+  }); // 10 seconds
+} else {
+  console.error('Image not found at:', imagePath);
+}
